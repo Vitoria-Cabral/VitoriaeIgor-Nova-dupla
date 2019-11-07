6f49b9dd32ab210686e36666ee0a87cfb20ab68f
Vitoria-Cabral <vitoriasilvacabral16@gmail.com>
2019-11-07 10:55:40 -0200

    vitoria

--- TP1_vitoriaeigorpronto/src/main/java/br/iff/edu/tp1_vitoriaeigorpronto/entidades/Usuario.java
+++ TP1_vitoriaeigorpronto/src/main/java/br/iff/edu/tp1_vitoriaeigorpronto/entidades/Usuario.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.iff.edu.tp1_vitoriaeigorpronto;
+package br.iff.edu.tp1_vitoriaeigorpronto.entidades;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
--- TP1_vitoriaeigorpronto/src/main/java/br/iff/edu/tp1_vitoriaeigorpronto/servlets/MeuprimeiroServlet.java
+++ TP1_vitoriaeigorpronto/src/main/java/br/iff/edu/tp1_vitoriaeigorpronto/servlets/MeuprimeiroServlet.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.iff.edu.tp1_vitoriaeigorpronto;
+package br.iff.edu.tp1_vitoriaeigorpronto.servlets;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
